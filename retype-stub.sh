72507173477ba827ceff06cde7005cd593644741
Fabrício Zimmerer Murta <fabricio@object.net>
2021-03-24 13:31:42 -0300

    Sets test to check against issue-1, and introduces a retype wrapper.
    It will just dump the provided commandline arguments to files to emulate
    a successful retype run, as if retype 1.2.0 was installed on the system.

---
+++ retype-stub.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+dest="$(echo "${@}" | egrep "\"output\":" | cut -f4 -d\")"
+
+if [ -z "${dest}" ]; then
+ echo "Ops, no dest. :S"
+ exit 1
+fi
+
+function dumparr() {
+ for i in "${@}"; do
+  echo -n " [\"${i}\"]"
+ done
+}
+
+result="- Commandline arguments:
+$(dumparr "${@}")
+- end of arguments list"
+
+if [ "${1}" == "--version" ]; then
+ echo "1.2.0"
+fi
+
+echo "${result}"
+
+if [ "${1}" == "init" ]; then
+ echo "Creating fake retype.json file"
+ echo "fake retype.json file with fake data.
+${result}" > "./retype.json"
+elif [ "${1}" == "build" ]; then
+ echo "Fake-building something."
+ if [ ! -d "${dest}" ]; then
+  mkdir -p "${dest}" || exit 1
+ fi
+ echo "fake index.html
+${result}" > "${dest}/index.html"
+else
+ echo "invalid command: ${1}"
+ exit 1
+fi
