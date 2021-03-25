62fd63f53f517a7daa4f4fa1c5aabffe34cea2c4
Retype GitHub Action <retype@object.net>
2021-03-25 18:07:31 +0000

    Refreshes Retype-generated documentation.
    Process triggered by fabriciomurta.

--- docs/resources/js/retype.js
+++ docs/resources/js/retype.js
@@ -1,9 +1,9 @@
 fake resources/js/retype.js
 - Commandline arguments:
  ["build"] ["--verbose"] ["--override"] ["{
-  "output": "/tmp/tmp.QWbyOaPMl1",
+  "output": "/tmp/tmp.VbuAY539ny",
   "base": "ragha-test",
   "title": "Coo\"lPr'oj",
-  "license": "Retype_Super Secret\n\nLicense key here! \"I like license keys\" \:D\n\nWith crazy line breaks etcetera. And all! Oh my, oh my! :P\"
+  "license": "Retype_Super Secret\n\nLicense key here! \"I like license keys\" \\:D\n\nWith crazy line breaks etcetera. And all! Oh my, oh my! :P\\"
 }"]
 - end of arguments list
