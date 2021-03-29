11e3f7f9b9a95deb1fb0b511afdc462c45f4d615
Retype GitHub Action <retype@object.net>
2021-03-29 15:39:44 +0000

    Refreshes Retype-generated documentation.
    Process triggered by fabriciomurta.

--- resources/js/retype.js
+++ resources/js/retype.js
@@ -1,10 +1,11 @@
 fake resources/js/retype.js
 - Commandline arguments:
  ["build"] ["--verbose"] ["--override"] ["{
-  "output": "/tmp/tmp.GrVS9K3CvN",
+  "output": "/tmp/tmp.vgCDi6jw4H",
   "base": "ragha-tests",
   identity {
     "title": "Coo\"lSt'aj"
-  }
+  },
+  "license": "some-test-license"
 }"]
 - end of arguments list
